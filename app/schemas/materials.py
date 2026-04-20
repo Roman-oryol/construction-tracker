8168c49c9a10827f89d57eb54828b27d3d88aa56
roman-oryol <roman.oryol83@gmail.com>
2026-04-20 22:09:21 +0300

    Добавить схемы для MaterialBase, MaterialCreate, MaterialUpdate и MaterialResponse

---
+++ app/schemas/materials.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class MaterialBase(BaseModel):
+    name: str
+    unit: str
+    low_stock_threshold: float = 0
+    description: str | None = None
+
+
+class MaterialCreate(MaterialBase):
+    project_id: int
+
+
+class MaterialUpdate(BaseModel):
+    name: str | None = None
+    unit: str | None = None
+    low_strock_threshold: float | None = None
+    description: str | None = None
+
+
+class MaterialResponse(MaterialBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    project_id: int
+    current_stock: float
