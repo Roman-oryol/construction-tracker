82a4033959662747bfbd1eb22ff86051d0772166
roman-oryol <roman.oryol83@gmail.com>
2026-04-21 10:00:01 +0300

    Удалить класс Base из database.py и добавить его в новый файл base.py с полями created_at и updated_at

--- app/database.py
+++ app/database.py
@@ -1,13 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
 engine = create_async_engine(settings.database_url)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
-
-
-class Base(DeclarativeBase):
-    pass
 
 
 async def get_db():
