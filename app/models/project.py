570b340d9b7259b337c425e19e5088c0071c0d8a
roman-oryol <roman.oryol83@gmail.com>
2026-04-21 08:41:57 +0300

    Добавить модель Project: name, address, owner_id, is_active

---
+++ app/models/project.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database import Base
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    address: Mapped[str | None] = mapped_column(String(255), default=None)
+    description: Mapped[str | None] = mapped_column(Text, default=None)
