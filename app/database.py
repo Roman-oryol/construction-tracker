96aa86b337279c59b88eb00a7d423edd3f90d580
roman-oryol <roman.oryol83@gmail.com>
2026-04-21 08:39:52 +0300

    Убрать параметр echo=True из создания асинхронного движка базы данных

--- app/database.py
+++ app/database.py
@@ -2,9 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
-engine = create_async_engine(
-    settings.database_url, echo=True
-)  # echo=True убрать после окончания проекта
+engine = create_async_engine(settings.database_url)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
