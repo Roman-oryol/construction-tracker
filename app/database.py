09133a876e5e328f34093d7b7fe9337fc0d3a2ed
roman-oryol <roman.oryol83@gmail.com>
2026-04-26 21:17:56 +0300

    Исправить импорт конфигурации в модуле базы данных

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from app.config import settings
+from app.core.config import settings
 
 engine = create_async_engine(settings.database_url)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
