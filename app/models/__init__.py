c09cc7e97869fda72881d27c69c04783ebe2a97f
roman-oryol <roman.oryol83@gmail.com>
2026-04-26 21:18:19 +0300

    Добавить импорт модели пользователя в инициализацию моделей

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.project import Project
 from app.models.material import Material
 from app.models.delivery import Delivery
 from app.models.consumption import Consumption
+from app.models.user import User
