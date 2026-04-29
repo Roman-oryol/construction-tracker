c4bd5cac80195fd7cc3a69b3741aeb205dff7733
roman-oryol <roman.oryol83@gmail.com>
2026-04-29 19:56:55 +0300

    Добавить связь между пользователями и участниками проектов в модели User

--- app/models/user.py
+++ app/models/user.py
@@ -6,6 +6,7 @@ from app.models.base import Base
 
 if TYPE_CHECKING:
     from app.models.project import Project
+    from app.models.project_member import ProjectMember
 
 
 class User(Base):
@@ -14,4 +15,6 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255))
+
     projects: Mapped[list["Project"]] = relationship(back_populates="owner")
+    memberships: Mapped[list["ProjectMember"]] = relationship(back_populates="user")
