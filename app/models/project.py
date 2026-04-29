538af63f31d8bcc23f44c692df2cbc274b337d71
roman-oryol <roman.oryol83@gmail.com>
2026-04-29 19:56:41 +0300

    Исправить порядок импорта и обновить отношения в модели проекта

--- app/models/project.py
+++ app/models/project.py
@@ -4,10 +4,10 @@ from sqlalchemy import String, Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.models.base import Base
 
-
 if TYPE_CHECKING:
     from app.models.material import Material
     from app.models.user import User
+    from app.models.project_member import ProjectMember
 
 
 class Project(Base):
@@ -17,6 +17,7 @@ class Project(Base):
     name: Mapped[str] = mapped_column(String(100))
     address: Mapped[str | None] = mapped_column(String(255), default=None)
     description: Mapped[str | None] = mapped_column(Text, default=None)
-    materials: Mapped[list["Material"]] = relationship(back_populates="project")
     owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     owner: Mapped["User"] = relationship(back_populates="projects")
+    materials: Mapped[list["Material"]] = relationship(back_populates="project")
+    members: Mapped[list["ProjectMember"]] = relationship(back_populates="project")
