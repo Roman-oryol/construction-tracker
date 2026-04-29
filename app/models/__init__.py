184863b033b6a011ef9d5b9077a979bcb6b1f4e6
roman-oryol <roman.oryol83@gmail.com>
2026-04-29 19:57:02 +0300

    Добавить импорт модели ProjectMember в инициализацию моделей

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.material import Material
 from app.models.delivery import Delivery
 from app.models.consumption import Consumption
 from app.models.user import User
+from app.models.project_member import ProjectMember
