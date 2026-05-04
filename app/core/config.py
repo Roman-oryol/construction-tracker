e82508ece86f02df0bb3913d3166936a45516065
roman-oryol <roman.oryol83@gmail.com>
2026-05-04 09:40:19 +0300

    Увеличить время истечения токена доступа с 30 до 60 минут

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     database_url: str
     secret_key: str
     algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
+    access_token_expire_minutes: int = 60
 
 
 settings = Settings()  # type: ignore
