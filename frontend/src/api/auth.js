3c43edf0f8961a8ed466bcb771bf2a5845a67f0d
roman-oryol <roman.oryol83@gmail.com>
2026-05-05 19:59:07 +0300

    feat: добавить функции регистрации и входа в систему в API

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,11 @@
+import client from './client'
+
+export const register = (email, password) => client.post('/auth/register', { email, password })
+
+export const login = async (email, password) => {
+  const form = new URLSearchParams()
+  form.append('username', email)
+  form.append('password', password)
+  const response = await client.post('/auth/login', form)
+  return response.data
+}
